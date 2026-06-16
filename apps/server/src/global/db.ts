b7cf50a60196231adc567d3a72888f917564e2b6
bamboo512 <weightwei@gmail.com>
2026-06-17 00:28:42 +0800

    refactor: migrate database connection to use persistent pg Pool for improved reliability

--- apps/server/src/global/db.ts
+++ apps/server/src/global/db.ts
@@ -1,14 +1,22 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "@/global/env";
-import * as schema from '../db/schema';
-import { Pool } from 'pg';
-import { relations } from '@/db/schema/relations';
+import * as schema from "../db/schema";
+import { Pool } from "pg";
+import { relations } from "@/db/schema/relations";
 
 if (!env.DB_URL) {
-    throw new Error('DB_URL is not set');
+    throw new Error("DB_URL is not set");
 }
 
+const pool = new Pool({
+    connectionString: env.DB_URL,
+});
 
-export const db = drizzle(env.DB_URL, {
-    relations
+pool.on("error", (err) => {
+    console.error("Unexpected error on idle database client", err);
+});
+
+export const db = drizzle({
+    client: pool,
+    relations,
 });
